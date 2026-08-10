e7c3b5d6bbcc572972a532cc3bd4b4c841ffb3b5
Arshit Gautam <arshitgautam09@gmail.com>
2026-08-11 03:42:58 +0530

    add all

---
+++ Practice/1.9/Data modelling.sql
@@ -0,0 +1,85 @@
+select
+job_id,
+company_id,
+salary_year_avg,
+job_title_short
+from 
+job_postings_fact
+limit 10;
+
+
+
+select *
+from information_schema.tables
+where 
+table_catalog='data_jobs';
+
+
+pragma show_tables_expanded;
+
+select 
+jpf.job_title_short,
+jpf.job_id,
+jpf.salary_year_avg not null,
+jpf.job_via,
+cd.company_id
+from 
+job_postings_fact as jpf 
+left join Company_dim as cd 
+on jpf.Company_id= cd.Company_id
+limit 10;
+
+
+
+select*
+from skills_dim
+limit 10;
+
+select*
+from skills_job_dim
+limit 10;
+
+
+
+
+select 
+job_postings_fact.*,
+skills_job_dim.*
+
+
+from job_postings_fact 
+inner join skills_job_dim 
+on job_postings_fact.job_id = skills_job_dim.job_id;
+
+
+
+select 
+jpf.job_title_short,
+jpf.job_id,
+sd.skill_id,
+sk.skills
+
+
+from job_postings_fact as jpf
+left join skills_job_dim as sd
+on jpf.job_id = sd.job_id
+left join skills_dim as sk 
+on sk.skill_id= sd.skill_id;
+ 
+
+
+ select 
+jpf.job_title_short,
+jpf.job_id,
+sd.skill_id,
+sk.skills
+
+
+from job_postings_fact as jpf
+Inner join skills_job_dim as sd
+on jpf.job_id = sd.job_id
+Inner join skills_dim as sk 
+on sk.skill_id= sd.skill_id;
+
+
+
