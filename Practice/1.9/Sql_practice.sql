8ef700c50a634b118807bccb5161f673e4431f08
Arshit Gautam <arshitgautam09@gmail.com>
2026-08-12 12:02:53 +0530

    Update for Sql_practice.sql

--- Practice/1.9/Sql_practice.sql
+++ Practice/1.9/Sql_practice.sql
@@ -1,4 +1,10 @@
 select 
-job_title_short
+job_title_short,
+job_country,
+salary_year_avg
 from
-job_postings_fact;
+job_postings_fact
+where 
+salary_year_avg is between 100000 and 200000
+AND 
+job_title_short ='Data Engineer'
